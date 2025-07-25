fee15307ecf7f5823f8a8f2c3e741fe05c6df875
srishylam burla <srishylamburla1@gmail.com>
2025-07-25 15:33:36 +0530

    Fix: production

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import uploadRoutes from './routes/uploadRoutes.js'
-import cookieParser from 'cookie-parser';
-import path from 'path'
-import { errorHandler, mongooseErrorHandler, notFound } from './middleware/errorMiddleware.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import {
+  errorHandler,
+  mongooseErrorHandler,
+  notFound,
+} from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -15,39 +21,43 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Middleware to parse JSON
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use('/api/products', productRoutes);
-app.use("/api/orders", orderRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/upload", uploadRoutes)
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/upload", uploadRoutes);
 
-app.get("/api/config/paypal", (req, res)=>res.send({
-    clientId: process.env.PAYPAL_CLIENT_ID
+app.get("/api/config/paypal", (req, res) =>
+  res.send({
+    clientId: process.env.PAYPAL_CLIENT_ID,
   })
-)
-const __dirname = path.resolve() 
-app.use("/uploads", express.static(path.join(__dirname, '/uploads')))
+);
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname,"/frontend/build")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  })
-}else{
-  app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+} else {
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
 }
-
 app.use(notFound);
-app.use(errorHandler)
-app.use(mongooseErrorHandler);  
+app.use(errorHandler);
+app.use(mongooseErrorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(
+    `\x1b[36m%s\x1b[0m`,
+    `Server is running on http://localhost:${PORT}`
+  );
 });
